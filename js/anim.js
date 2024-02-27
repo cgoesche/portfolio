af5207b9d71d03afcc0fab7e87a2814113c1c5e9
christiangoeschel <cndjomouo@icloud.com>
2024-02-27 10:23:19 -0500

    Initial commit

---
+++ js/anim.js
@@ -0,0 +1,12 @@
+//Animation function for bottomright_message 
+function changePos(id) {
+        $(
+                id).style.bottom = "-100%";
+    }
+
+//Timer for loading cover
+setTimeout(function(){
+        $('.loading_cover').addClass('loaded')
+},3000);
+
+
